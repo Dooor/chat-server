e4f498b897fabc58c65d970d4d68b5e0283c5e0b
Dooor <dooor1109@gmail.com>
2017-05-05 17:14:50 +0900

    chatsテーブルを作成するsqlスクリプト追加

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,12 @@
+# chatsテーブルの作成
+
+# --- !Ups
+CREATE TABLE IF NOT EXISTS chats(
+  id int(11) NOT NULL AUTO_INCREMENT,
+  body varchar(255) NOT NULL,
+  sender_name varchar(255) NOT NULL,
+  PRIMARY KEY(id)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+# --- !Downs
+DROP TABLE IF EXISTS chats;
