0a8a925e463899536fe881bd9b560a42b140330f
Dooor <dooor1109@gmail.com>
2017-05-07 18:25:30 +0900

    chatsテーブルにsent_atカラムを追加

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -5,6 +5,7 @@ CREATE TABLE IF NOT EXISTS chats(
   id int(11) NOT NULL AUTO_INCREMENT,
   body varchar(255) NOT NULL,
   sender_name varchar(255) NOT NULL,
+  sent_at datetime(6) NOT NULL,
   PRIMARY KEY(id)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
