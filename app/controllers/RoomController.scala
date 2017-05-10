b2b5f70ec2b9e4a334fe179ff72630bfacb02045
Dooor <dooor1109@gmail.com>
2017-05-10 17:14:09 +0900

    Add RoomController#index

--- app/controllers/RoomController.scala
+++ app/controllers/RoomController.scala
@@ -1,12 +1,19 @@
 package controllers
 
+import domains.room.RoomEntityJsonProtocol._
 import javax.inject._
 import play.api.libs.json.Json
 import play.api.mvc._
 import services.room.RoomService
+import spray.json._
 
 @Singleton
 class RoomController @Inject() (roomService: RoomService) extends Controller {
+  def index: Action[AnyContent] = Action {
+    val result = roomService.listAll()
+    Ok(result.toJson.prettyPrint).as(JSON)
+  }
+
   def create = Action(parse.json) { request =>
     val name = (request.body \ "name").as[String]
     roomService.create(name)
