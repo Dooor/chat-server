479c8a1a947fdb2b3f453544af96fa269a566bed
Dooor <dooor1109@gmail.com>
2017-05-10 17:06:45 +0900

    Make RoomController#create insert record to DB.

--- app/controllers/RoomController.scala
+++ app/controllers/RoomController.scala
@@ -3,11 +3,13 @@ package controllers
 import javax.inject._
 import play.api.libs.json.Json
 import play.api.mvc._
+import services.room.RoomService
 
 @Singleton
-class RoomController @Inject() extends Controller {
+class RoomController @Inject() (roomService: RoomService) extends Controller {
   def create = Action(parse.json) { request =>
     val name = (request.body \ "name").as[String]
+    roomService.create(name)
 
     val result = Map("status" -> "ok", "method" -> "POST")
     Ok(Json.toJson(result))
